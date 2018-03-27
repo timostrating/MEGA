31387656e3e3eb75aa37127fa44ea3620d05d93a
Timo Strating <timostrating@home.nl>
2018-03-27 23:31:16 +0200

    test

---
+++ test.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox()
+
+# go to the google home page
+driver.get("http://www.hanze.nl/my.policy")
+
+# the page is ajaxy so the title is originally this:
+print driver.title
+
+
+try:
+    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+
+    # You should see "cheese! - Google Search"
+    print driver.title
+
+finally:
+    driver.quit()
+
