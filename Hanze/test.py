b0d9e3ef1b44e803dd04b54ba577bc2e8013c9f9
Timo Strating <timostrating@home.nl>
2018-04-03 22:53:22 +0200

    automated login

--- Hanze/test.py
+++ Hanze/test.py
@@ -2,15 +2,38 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.keys import Keys
+
+username = "______"
+password = "______"  
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
 # go to the google home page
-driver.get("http://www.hanze.nl/my.policy")
+driver.get("http://www.hanze.nl")
 
 # the page is ajaxy so the title is originally this:
-print driver.title
+print " -- " + driver.title
+
+login_button = driver.find_element_by_css_selector('a.ng-binding')
+login_button.click()
+
+print " -- " + driver.title
+
+
+username_field = driver.find_element_by_id('input_1')
+username_field.clear()
+username_field.send_keys(username)
+
+password_field = driver.find_element_by_id('input_2')
+password_field.clear()
+password_field.send_keys(password)
+
+submit_button = driver.find_element_by_css_selector('input.credentials_input_submit')
+submit_button.click()
+
+print driver.get_cookies()
 
 
 try:
@@ -22,4 +45,7 @@ try:
 
 finally:
     driver.quit()
+
+
+
 
